53232608c8900ed40ee7a73dd8b88ed05ca29046
Leonardo <leonardomullerdelima@pm.me>
2025-09-11 15:02:27 -0300

    feat: add home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,67 @@
-const Home = ({ nome }) => {
+import { useState, useEffect } from "react";
+import { useOutletContext, NavLink } from "react-router-dom";
+import "../styles/home.css";
+
+import homeImage from "../assets/heshan-perera-Q1AhQ2DgSQA-unsplash.jpg";
+
+const Home = () => {
+  const { products, loading, error } = useOutletContext();
+  const [mensClothing, setMensClothing] = useState([]);
+  const [womenClothing, setWomenClothing] = useState([]);
+
+  useEffect(() => {
+    setMensClothing(filterProducts("men's clothing"));
+    setWomenClothing(filterProducts("women's clothing"));
+  }, [products]);
+
+  function filterProducts(category) {
+    return products.filter((p) => p.category === category);
+  }
+
   return (
-    <>
-      <h2>Home</h2>
-      <p>asjdasdjad {nome}</p>
-    </>
+    <section className="home-section">
+      <div className="image-container">
+        <img src={homeImage} alt="" />
+        <p>Thankyou for buying with us.</p>
+      </div>
+      <main className="home-main">
+        <div>
+          <p>
+            Check out the best products, tested and approved by specialists.
+          </p>
+        </div>
+        <div className="home-products-line">
+          <h2>Popular men clothing</h2>
+          <div className="home-products">
+            {mensClothing.map((p, index) => (
+              <div key={p.id}>
+                <img src={p.image} alt="Product" />
+                <h3>{p.title}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="home-products-line">
+          <h2>Popular women clothing</h2>
+          <div className="home-products">
+            {womenClothing.map((p, index) => (
+              <div key={p.id}>
+                <img src={p.image} alt="Product" />
+                <h3>{p.title}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="shop-button-container">
+          <button className="shop-button">
+            <NavLink to="/shop">See all our products</NavLink>
+          </button>
+        </div>
+        <div>
+          <p>This is a fake store page created to practice SPAs.</p>
+        </div>
+      </main>
+    </section>
   );
 };
 
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,12 +1,26 @@
+import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 
 import "../styles/shop.css";
 
 const Shop = () => {
   const { products, loading, error } = useOutletContext();
+  const [fieldAmount, setFieldAmount] = useState([]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setFieldAmount(new Array(products.length).fill(1));
+    }
+  }, [products]);
+
+  function handleChange(e, index) {
+    const newAmount = [...fieldAmount];
+    newAmount[index] = parseInt(e.target.value) || 1;
+    setFieldAmount(newAmount);
+  }
 
   return (
     <section className="shop-section">
@@ -19,7 +33,14 @@ const Shop = () => {
             <div className="shop-add-to-cart-section">
               <div className="shop-add-to-cart-buttons">
                 <button></button>
-                <input type="tel" />
+                <input
+                  type="number"
+                  min="1"
+                  name="amount"
+                  id="amount"
+                  value={fieldAmount[index]}
+                  onChange={(e) => handleChange(e, index)}
+                />
                 <button></button>
               </div>
               <button>Add to Cart</button>
