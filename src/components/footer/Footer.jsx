15449360148fd05454e7b9184973af8227671305
Leonardo Muller de Lima <leonardomullerdelima@pm.me>
2026-01-19 23:19:03 -0300

    feat: add initial footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles["main"]}>
+            <p>
+                &copy; Lemü
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
