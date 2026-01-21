88c4eefe6d9ef1b352a879362ded0edc17943002
Leonardo Muller de Lima <leonardomullerdelima@pm.me>
2026-01-20 21:22:31 -0300

    feat: install and setup package for testing

---
+++ tests/test-utils.jsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+const AllTheProviders = ({ children }) => {
+  return (
+    <MemoryRouter>
+      {children}
+    </MemoryRouter>
+  );
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
